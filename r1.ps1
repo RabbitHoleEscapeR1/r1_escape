5e55055dae767cb5dbd8ed11577bce1f3c0ad0e6
Kshitij Gupta <kshitijgm@gmail.com>
2024-05-31 17:10:39 +0530

    r1-ps1: Install chocolatey as admin

--- r1.ps1
+++ r1.ps1
@@ -1,7 +1,7 @@
 # Function to install Chocolatey if not installed
 function Install-Chocolatey {
     if (-not (Test-Path "$env:ProgramData\chocolatey")) {
-        Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+        Start-Process powershell.exe -Verb RunAs -ArgumentList "-Command &{ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) }"
     }
 }
 
