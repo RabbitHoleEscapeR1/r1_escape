3af6927bbedd45ddbfe4831853eb2fdd45aa89e8
Kshitij Gupta <kshitijgm@gmail.com>
2024-06-01 11:03:14 +0530

    r1_escape-mtkbootcmd: Don't try to init all serial ports, match hwid

--- mtkbootcmd.py
+++ mtkbootcmd.py
@@ -4,52 +4,41 @@ import sys
 import time
 import glob
 import serial
+import serial.tools.list_ports
 
-def serial_ports():
-    if sys.platform.startswith('win'):
-        _ports = ['COM%s' % (i + 1) for i in range(256)]
-    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        _ports = glob.glob('/dev/tty[A-Za-z]*')
-    elif sys.platform.startswith('darwin'):
-        # Don't accidentally look up wlan-debug and bluetooth ports
-        _ports = glob.glob('/dev/tty.usbmodem*')
-    else:
-        raise EnvironmentError('Unsupported platform')
-
-    result = []
-    for port in _ports:
-        try:
-            s = serial.Serial(port)
-            s.close()
-            result.append(port)
-        except (OSError, serial.SerialException):
-            pass
-    return result
-
-
+PLBROM_HWID = "VID:PID=0E8D:2000"
 BOOTSEQ = bytes(sys.argv[1], "ascii")
 READYCMD = b"READY"
+
+
+def serial_port():
+    ports = list(serial.tools.list_ports.comports())
+    for port in ports:
+        if PLBROM_HWID in port.hwid:
+            print("Found {} with description: {}\nHWID: {}".format(port.device, port.description, port.hwid))
+            return port.device
+    return None
+
 
 if __name__ == '__main__':
     print('Listening for ports!')
     abort = False
     while not abort:
         time.sleep(1)
-        ports = serial_ports()
-        if len(ports) > 0:
-            print('Got ports:', ports)
-            for port in ports:
-                print('Initializing port', port)
-                ser = serial.Serial(port=port, baudrate=115200)
-                try:
-                    resp = ser.read(5)
-                    if resp == READYCMD:
-                        ser.write(BOOTSEQ)
-                        print("{} cmd sent".format(BOOTSEQ))
-                        abort = True
-                        break
-                    else:
-                        raise Exception()
-                except:
-                    print('No READY signal, next port.')
-                    continue
+        port = serial_port()
+        if port is not None:
+            print('Got port:', port)
+            print('Initializing port', port)
+            ser = serial.Serial(port=port, baudrate=115200)
+            try:
+                resp = ser.read(5)
+                if resp == READYCMD:
+                    ser.write(BOOTSEQ)
+                    print("{} cmd sent".format(BOOTSEQ))
+                    abort = True
+                    break
+                else:
+                    raise Exception()
+            except:
+                print('No READY signal, next port.')
+                continue
